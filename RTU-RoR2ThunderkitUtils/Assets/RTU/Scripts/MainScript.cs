c45d301648ef9d1180270d7f248ee2b970266dec
Nebby <nebby131999@gmail.com>
2021-07-03 13:43:57 -0400

    First Commit!

---
+++ RTU-RoR2ThunderkitUtils/Assets/RTU/Scripts/MainScript.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTU
+{
+    [BepInPlugin(guid, modName, version)]
+    public class MainScript : BaseUnityPlugin
+    {
+        internal const string guid = "com.ValeXNebby.RTU";
+        internal const string modName = "RoR2ThunderkitUtils";
+        internal const string version = "0.0.1";
+    }
+}
